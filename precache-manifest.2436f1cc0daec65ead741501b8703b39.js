804e7b71e8279b388de310141bc051cd918abdb0
lingaraj s <lingarajs25@gmail.com>
2021-05-30 15:53:50 +0530

    Updates

---
+++ precache-manifest.2436f1cc0daec65ead741501b8703b39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41f94a94302ab9b0808422087ad18616",
+    "url": "/Appiness-task/index.html"
+  },
+  {
+    "revision": "cfc040264e97f5e62037",
+    "url": "/Appiness-task/static/css/2.1468f438.chunk.css"
+  },
+  {
+    "revision": "2de29ae1fe1573524a69",
+    "url": "/Appiness-task/static/css/main.eb779aee.chunk.css"
+  },
+  {
+    "revision": "cfc040264e97f5e62037",
+    "url": "/Appiness-task/static/js/2.d742555d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Appiness-task/static/js/2.d742555d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2de29ae1fe1573524a69",
+    "url": "/Appiness-task/static/js/main.56ad66b7.chunk.js"
+  },
+  {
+    "revision": "3da9e430a92084a3a304",
+    "url": "/Appiness-task/static/js/runtime-main.12e6eb69.js"
+  }
+]);
